3c51051096988dbe98e77e8c447113735385245c
Abdualim Normurodov <abdualimnormurodov1@gmail.com>
2023-10-31 21:56:56 +0500

    Add Student

---
+++ Students.cs
@@ -0,0 +1,10 @@
+﻿namespace AllTech
+{
+    public class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
